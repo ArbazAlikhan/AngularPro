12689618df2937266f93e68e29c7c7f62532f4de
Khan <859781@cognizant.com>
2020-08-30 15:21:09 +0530

    Appsheader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular';
+
+  componentTitle = "Welcome to Angular";
+
+  clickHandler(){
+    alert("I am Angular");
+  }
 }
